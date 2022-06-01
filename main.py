5b8d8c82df1e632ea878044286568d87604e6a17
travisback <travisback@icloud.com>
2022-05-31 21:44:03 -0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Band Name Generator (v1.0)\n")
+
+city = input("What city did you grow up in? ")
+pet = input("What is the name of your pet? ")
+
+print("")
+print("Your band name is: " + city + " " + pet)
